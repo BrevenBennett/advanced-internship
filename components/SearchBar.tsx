29b37039fababefd93849439d8554d20e6ad42bf
BrevenBennett <Breven.stars@gmail.com>
2024-02-06 13:36:37 -0600

    finished styling foryou page

---
+++ components/SearchBar.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { IoIosSearch } from "react-icons/io";
+
+export default function SearchBar() {
+  return (
+    <div className="searchbar">
+      <div className="searchbar__wrapper">
+        <input
+          className="search__input"
+          type="text"
+          placeholder="Search for books"
+        />
+        <div className="search__icon">
+          <IoIosSearch />
+        </div>
+      </div>
+    </div>
+  );
+}
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,14 +7,59 @@ import { LuLogOut } from "react-icons/lu";
 
 export default function Sidebar() {
   return (
-    <div>
-      <TiHomeOutline />
-      <CiBookmark />
-      <BsPen />
-      <IoIosSearch />
-      <CiSettings />
-      <IoIosHelpCircleOutline />
-      <LuLogOut />
+    <div className="sidebar">
+      <figure className="sidebar__img--mask">
+        <img className="sidebar__img" src="/assets/logo.png" alt="logo" />
+      </figure>
+      <div className="sidebar__wrapper">
+        <div className="sidebar__top">
+          <SidebarLink Icon={TiHomeOutline} text={"For you"} className="" link="/for-you" />
+          <SidebarLink Icon={CiBookmark} text={"My Library"} className="" link="/library" />
+          <SidebarLink
+            Icon={BsPen}
+            text={"Highlights"}
+            className={"sidebar__link--not-allowed"}
+            link=""
+          />
+          <SidebarLink
+            Icon={IoIosSearch}
+            text={"Search"}
+            className={"sidebar__link--not-allowed"}
+            link=""
+          />
+        </div>
+        <div className="sidebar__bottom">
+          <SidebarLink Icon={CiSettings} text={"Settings"} className="" link="/settings" />
+          <SidebarLink
+            Icon={IoIosHelpCircleOutline}
+            text={"Help & Support"}
+            className={"sidebar__link--not-allowed"}
+            link=""
+          />
+          <SidebarLink Icon={LuLogOut} text={"Logout"} className="" link="" />
+        </div>
+      </div>
     </div>
+  );
+}
+
+function SidebarLink({
+  text,
+  Icon,
+  className,
+  link,
+}: {
+  text: string;
+  Icon: any;
+  className: string;
+  link: string;
+}) {
+  return (
+    <>
+      <a href={link} className={`sidebar__link--wrapper ${className}`}>
+        <Icon className="sidebar__link--icon" />
+        <span className="sidebar__link--text">{text}</span>
+      </a>
+    </>
   );
 }

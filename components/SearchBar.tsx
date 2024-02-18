2a70539065f81ac8b86df593bf8fd2c7a331c7a2
BrevenBennett <Breven.stars@gmail.com>
2024-02-17 18:33:32 -0600

    search bar skeleton

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -7,13 +7,17 @@ import { GoClock } from "react-icons/go";
 import { IoIosSearch } from "react-icons/io";
 import { IoCloseSharp } from "react-icons/io5";
 import { useSelector } from "react-redux";
+import Skeleton from "./Skeleton";
 
 export default function SearchBar() {
   const [searchTerm, setSearchTerm] = useState<FormDataEntryValue | null>("");
   const [results, setResults] = useState<[]>([]);
   const [isSearching, setIsSearching] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState(false)
   const debouncedSearchTerm = useDebounce(searchTerm, 300);
-  const duration = useSelector((state: RootState) => state.audioPlayer.duration)
+  const duration = useSelector(
+    (state: RootState) => state.audioPlayer.duration
+  );
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(e.target.value);
@@ -32,6 +36,7 @@ export default function SearchBar() {
     const search = async () => {
       let results = [];
       if (debouncedSearchTerm) {
+        setIsLoading(true)
         const { data } = await axios.get(
           `https://us-central1-summaristt.cloudfunctions.net/getBooksByAuthorOrTitle?search=${debouncedSearchTerm}`
         );
@@ -40,6 +45,9 @@ export default function SearchBar() {
 
       setResults(results);
       console.log(results);
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 1000)
     };
 
     search();
@@ -63,26 +71,48 @@ export default function SearchBar() {
         </button>
         {searchTerm && isSearching && (
           <div className="search__books--wrapper">
-            {results.map((result: any) => (
-              <a
-                key={result.id}
-                href={`/book/${result.id}`}
-                className="search__book--link"
-              >
-                <audio src={result.audioLink}></audio>
-                <figure className="search-book__image--wrapper">
-                  <img src={result.imageLink} alt="" className="book__image" />
-                </figure>
-                <div>
-                  <div className="search__book--title">{result.title}</div>
-                  <div className="search__book--author">{result.author}</div>
-                  <div className="recommended__books--details">
-                    <GoClock className="recommended__books--details-icon" />
-                    <div className="recommended__books--details-text">{duration}</div>
+            {results.length > 0 ? (
+              isLoading ? 
+              <>
+                  <Skeleton className="search__book--link" width="100%" height="120px" />
+                  <br />
+                  <Skeleton className="search__book--link" width="100%" height="120px" />
+                  <br />
+                  <Skeleton className="search__book--link" width="100%" height="120px" />
+                  <br />
+                  <Skeleton className="search__book--link" width="100%" height="120px" />
+                  <br />
+                  <Skeleton className="search__book--link" width="100%" height="120px" />
+              </>
+               : results.map((result: any) => (
+                <a
+                  key={result.id}
+                  href={`/book/${result.id}`}
+                  className="search__book--link"
+                >
+                  <audio src={result.audioLink}></audio>
+                  <figure className="search-book__image--wrapper">
+                    <img
+                      src={result.imageLink}
+                      alt=""
+                      className="book__image"
+                    />
+                  </figure>
+                  <div>
+                    <div className="search__book--title">{result.title}</div>
+                    <div className="search__book--author">{result.author}</div>
+                    <div className="recommended__books--details">
+                      <GoClock className="recommended__books--details-icon" />
+                      <div className="recommended__books--details-text">
+                        {duration}
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </a>
-            ))}
+                </a>
+              ))
+            ) : (
+              <div className="">No Books Found</div>
+            )}
           </div>
         )}
       </form>
